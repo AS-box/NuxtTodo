caede149c1d9fef4327341ab7d43e9e79dd42dee
ai-tanaka629 <ai.tanaka@webcrew.co.jp>
2025-02-28 13:58:07 +0900

    prismaを公開サーバーにあげた

---
+++ Todo/pages/api/tasks.sort.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from "@prisma/client";
+
+export default defineEventHandler(async (event) => {
+    const prisma = new PrismaClient();
+    const body = await readBody(event);
+    let tasks;
+    if(body.sort === '4'){
+        tasks = await prisma.task.findMany();
+    }else{
+        tasks = await prisma.task.findMany({
+            where:{
+                status: body.sort
+            }
+        });
+    }
+    console.log(tasks);
+    return tasks;
+});
